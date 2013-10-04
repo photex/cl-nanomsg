e64bb17ca9d89b15e8c8c1f506cb7e1629bf2b50
Chip Collier <photex@lofidelitygames.com>
2013-10-03 21:08:34 -0700

    - added dependency on conspack to the plant project settings

--- plant-project.scm
+++ plant-project.scm
@@ -3,9 +3,14 @@
   "alexandria"
   "osicat"
   "cl-fad"
-  "cl-autowrap")
+  "cl-autowrap"
+  "cl-conspack")
  (#:project-name . "cl-nanomsg")
  (#:project-version . "0.0.1")
  (#:dependencies
+  ((#:method . "git")
+   (#:url
+    .
+    "git://github.com/conspack/cl-conspack.git"))
   ((#:method . "git")
    (#:url . "git://github.com/rpav/cl-autowrap.git"))))
